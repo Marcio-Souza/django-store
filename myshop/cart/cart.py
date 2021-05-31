d82bd600ae5cc213b8ed72594d991fc935a5812b
marcio <wache2k@gmail.com>
2021-05-31 09:02:49 -0300

    Update with flake8

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -14,10 +14,14 @@ class Cart(object):
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
-        """"Adiciona um produto no carrinho de compras ou atualiza a quantidade"""
+        """"
+        Adiciona um produto no carrinho de compras
+        ou atualiza a quantidade
+        """
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
         if override_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -28,16 +32,17 @@ class Cart(object):
         # marca a sessao como 'modificada' para garantir que ela seja salva
         self.session.modified = True
 
-    def remove(self):
+    def remove(self, product):
         """Remove um item do carrinho"""
-        product_id = str(product_id)
+        product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
 
     def __iter__(self):
         """
-        Itera pelos itens do carrinho de compras e obtem os produtos do banco de dados
+        Itera pelos itens do carrinho de compras
+        e obtem os produtos do banco de dados
         """
         product_ids = self.cart.keys()
         # obtém os objetos referentes aos produtos e os adiciona no carrinha
@@ -57,7 +62,8 @@ class Cart(object):
 
     def get_total_price(self):
         """Soma o total dos pedido"""
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+        return sum(Decimal(item['price']) * item['quantity']
+                   for item in self.cart.values())
 
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
